462b0d5a5b89c54c33393e79f17f03b32d3c6cd4
FrancescoPietri <francescopietri1@gmail.com>
2025-11-06 14:22:59 +0100

    Sync lora

--- MqttSubscriber.py/Lora_rx_test.ino
+++ MqttSubscriber.py/Lora_rx_test.ino
@@ -1,32 +1,56 @@
-
 #include <SPI.h>  
 #include <LoRa.h> 
 
-#define LORA_CS_PIN     10 
-#define LORA_RESET_PIN  9 
-#define LORA_IRQ_PIN    2   
+#define LORA_CS_PIN     10  // Chip Select (NSS)
+#define LORA_RESET_PIN  9   // Reset (RST)
+#define LORA_IRQ_PIN    2   // Interrupt (DIO0)
+
+String tx_msg = ""; 
 
 void setup() {
+  // Start Serial for debugging
   Serial.begin(9600);
   while (!Serial); 
   
   LoRa.setPins(LORA_CS_PIN, LORA_RESET_PIN, LORA_IRQ_PIN);
-  
+
   if (!LoRa.begin(433E6)) { 
-    while (1);
+    while (1); 
   }
 }
 
 void loop() {
-    int packetSize = LoRa.parsePacket();
+    
+   LoRa.receive();
+   
+  int packetSize = LoRa.parsePacket();
   
-    if (packetSize) {
+  if (packetSize) {
     String receivedData = "";
     while (LoRa.available()) {
       receivedData += (char)LoRa.read();
     }
 
-    Serial.print(receivedData);
-    Serial.println("");
+    if (receivedData == "ping") {
+      
+      if (tx_msg.length() > 0) {
+
+        LoRa.beginPacket();
+        LoRa.print(tx_msg);
+        LoRa.endPacket();
+        
+      }
+
+      tx_msg = ""; 
+    }
+  } 
+
+  if (Serial.available() > 0) {
+    String newMessage = Serial.readStringUntil('\n');
+    newMessage.trim();
+
+    if (newMessage.length() > 0) {
+      tx_msg = newMessage;
+    }
   }
 }

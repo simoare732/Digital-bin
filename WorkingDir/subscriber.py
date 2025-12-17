675d38b21fa5004c649db19682eb36c26de57868
FrancescoPietri <francescopietri1@gmail.com>
2025-12-17 12:04:20 +0100

    working on thingsboard

--- WorkingDir/subscriber.py
+++ WorkingDir/subscriber.py
@@ -3,6 +3,8 @@ import ssl
 import os
 from pathlib import Path
 import math
+import requests
+import json
 
 BROKER_HOST = "3224d9e30f954d01a9b9570ad77953f2.s1.eu.hivemq.cloud" 
 PORT = 8883 
@@ -10,8 +12,9 @@ CLIENT_ID = "subscriber_1"
 USERNAME = "bridge" 
 TOPIC_TO_SUBSCRIBE= "hivemq/ahfgnsad439/BINs/#"
 SUBSCRIBED_DATA = {} 
+GATEWAY_TOKEN = ""
 
-MAX_FILL = 90
+MAX_FILL = 50
 
 def read_password_from_file(file_name):
     try:
@@ -79,6 +82,38 @@ def bin_distance(lat1, lon1, lat2, lon2):
     
     return distance
 
+def publish_gateway_data(gateway_token, bin_id, telemetries):
+    """
+    Invia dati a ThingsBoard tramite un unico Gateway.
+    Format: {"NomeCestino": [{"key": "value"}, ...]}
+    """
+    script_dir = Path(__file__).parent.resolve()
+    password_file = script_dir / "token_tib"
+    GATEWAY_TOKEN = read_password_from_file(password_file)
+    print(GATEWAY_TOKEN)
+
+    url = f"https://thingboard.faffofvtt.work:443/api/v1/{GATEWAY_TOKEN}/telemetry"
+    headers = {'Content-Type': 'application/json'}
+    
+    # Formato richiesto dal Gateway di ThingsBoard
+    payload = {
+        bin_id: [
+            {
+                "ts":None,
+                "values":  telemetries
+            }
+        ]
+    }
+    
+    try:
+        response = requests.post(url, data=json.dumps(payload), headers=headers, timeout=5)
+        if response.status_code == 200:
+            print(f"Dati Gateway inviati: {bin_id} -> ThingsBoard")
+        else:
+            print(f"Errore Gateway: {response.status_code} - {response.text}")
+    except Exception as e:
+        print(f"Errore connessione Gateway: {e}")
+
 def on_message(client, userdata, msg):
     """Callback new message"""
     bin_id = msg.topic.split("/")[-2]
@@ -91,13 +126,21 @@ def on_message(client, userdata, msg):
         client.publish(f"hivemq/ahfgnsad439/BINs/{bin_id}/lcd", payload="0,ok", qos=1)
         SUBSCRIBED_DATA[bin_id]["lock"] = 0
         SUBSCRIBED_DATA[bin_id]["lcd"] = "0,ok"
+        telemetries={"lock":0}
+        publish_gateway_data(GATEWAY_TOKEN, bin_id, telemetries)    
+        telemetries={"lcd":"0,ok"}
+        publish_gateway_data(GATEWAY_TOKEN, bin_id, telemetries)    
 
     SUBSCRIBED_DATA[bin_id][data_key] = payload_value
+    telemetries={data_key:payload_value}
+    publish_gateway_data(GATEWAY_TOKEN, bin_id, telemetries)
 
     if data_key == "fill":
         if int(payload_value) >= MAX_FILL and int(SUBSCRIBED_DATA[bin_id]["lock"])==0:
             client.publish(f"hivemq/ahfgnsad439/BINs/{bin_id}/lock", payload=1, qos=1)
             SUBSCRIBED_DATA[bin_id]["lock"] = 1
+            telemetries={"lock":1}
+            publish_gateway_data(GATEWAY_TOKEN, bin_id, telemetries)  
             nextBinDistace=1000
             direction="none"
             for key in SUBSCRIBED_DATA:
@@ -109,16 +152,26 @@ def on_message(client, userdata, msg):
                             direction = next_bin_direction(float(SUBSCRIBED_DATA[bin_id]["lat"]), float(SUBSCRIBED_DATA[bin_id]["lon"]), float(SUBSCRIBED_DATA[key]["lat"]), float(SUBSCRIBED_DATA[key]["lon"]))
                         
             if nextBinDistace < 1000:
-                client.publish(f"hivemq/ahfgnsad439/BINs/{bin_id}/lcd", payload=f"{nextBinDistace},{direction}", qos=1)
+                nextBinDistace=round(nextBinDistace, 0)
+                client.publish(f"hivemq/ahfgnsad439/BINs/{bin_id}/lcd", payload=f"{nextBinDistace, 0},{direction}", qos=1)
                 SUBSCRIBED_DATA[bin_id]["lcd"] = f"{nextBinDistace},{direction}"
+                telemetries={"lcd":f"{nextBinDistace},{direction}"}
+                publish_gateway_data(GATEWAY_TOKEN, bin_id, telemetries)  
             else:
                 client.publish(f"hivemq/ahfgnsad439/BINs/{bin_id}/lcd", payload="0,ok", qos=1)
                 SUBSCRIBED_DATA[bin_id]["lcd"] = "0,ok"
+                telemetries={"lcd":"0,ok"}
+                publish_gateway_data(GATEWAY_TOKEN, bin_id, telemetries)   
+
         elif(int(SUBSCRIBED_DATA[bin_id]["lock"])==1 and int(SUBSCRIBED_DATA[bin_id]["fill"])<MAX_FILL):
             client.publish(f"hivemq/ahfgnsad439/BINs/{bin_id}/lock", payload=0, qos=1)
             client.publish(f"hivemq/ahfgnsad439/BINs/{bin_id}/lcd", payload="0,ok", qos=1)
             SUBSCRIBED_DATA[bin_id]["lock"] = 0
             SUBSCRIBED_DATA[bin_id]["lcd"] = "0,ok"
+            telemetries={"lock":0}
+            publish_gateway_data(GATEWAY_TOKEN, bin_id, telemetries)    
+            telemetries={"lcd":"0,ok"}
+            publish_gateway_data(GATEWAY_TOKEN, bin_id, telemetries)    
 
     print(SUBSCRIBED_DATA)
     print("\n")
@@ -131,9 +184,11 @@ def main():
     script_dir = Path(__file__).parent.resolve()
     password_file = script_dir / "token.txt"
     PASSWORD = read_password_from_file(password_file)
+
     if not PASSWORD:
         print(f"Error password can not be null")
         return -1
+
 
     client = mqtt.Client(client_id=CLIENT_ID, protocol=mqtt.MQTTv311)
     client.on_connect = on_connect
